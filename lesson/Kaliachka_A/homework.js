fa2d2bcf18c295d1463a6cc00652bf0793435430
Kaliachka_A <akaliach@es.ibm.com>
2018-10-24 17:43:58 +0300

    implement isInArray function

--- lesson/Kaliachka_A/homework.js
+++ lesson/Kaliachka_A/homework.js
@@ -1,9 +1,19 @@
 "use strict";
-/*
-function isInArray(): boolean {
-    return false;
+/**
+ * Method to check if all arguments exist in array or not
+ * @param array - array to check
+ * @param  args - arguments, which should exist in array
+ * @returns {boolean} - true if all args in array, false otherwise
+ */
+function isInArray(array, ...args) {
+    let result = true;
+    args.forEach((arg) => {
+        if (array.indexOf(arg) === -1) {
+            result = false;
+        }
+    });
+    return result;
 }
-*/
 /**
  * Method to calculate sum of input parameters
  * @param {(string | number)[]} args - variable number of parameters
--- lesson/Kaliachka_A/homework.ts
+++ lesson/Kaliachka_A/homework.ts
@@ -1,17 +1,28 @@
-/*
-function isInArray(): boolean {
-    return false;
+/**
+ * Method to check if all arguments exist in array or not
+ * @param array - array to check
+ * @param  args - arguments, which should exist in array
+ * @returns {boolean} - true if all args in array, false otherwise
+ */
+function isInArray<T>(array: T[], ...args: T[]): boolean {
+    let result: boolean = true;
+    args.forEach((arg: T) => {
+        if (array.indexOf(arg) === -1) {
+            result = false;
+        }
+    });
+    return result;
 }
-*/
+
 
 /**
  * Method to calculate sum of input parameters
  * @param {(string | number)[]} args - variable number of parameters
  * @returns {number} - sum of input parameters
  */
-function summator(...args: (string|number)[]): number {
+function summator(...args: (string | number)[]): number {
     let sum: number = 0;
-    args.forEach((arg: string|number) => {
+    args.forEach((arg: string | number) => {
             if (typeof arg === 'string') {
                 sum += parseInt(arg) || 0;
             } else {
