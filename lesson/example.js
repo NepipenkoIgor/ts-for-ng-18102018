77b0ae837a7b88787d213d747ae6de5433e4a0af
Liamin Serhii <liamin.web@gmail.com>
2018-10-25 22:30:02 +0300

    HW1

--- lesson/example.js
+++ lesson/example.js
@@ -1,40 +1,32 @@
-"use strict";
-Object.defineProperty(window, 'MySweetApp', {
-    value: 'v1.0.0',
-    writable: true,
-});
+Object.defineProperty(window, "MySweetApp", { value: "v1.0.0", readonly: false });
+
 function deliveryMethod() {
-    // TODO
-    return 'overnight';
+  // TODO
+  return "overnight";
 }
+
 function shipWeight() {
-    var el = document.getElementById('weight');
-    var width = '0';
-    if (el) {
-        width = el.innerHTML;
-    }
-    return parseInt(width);
+  return parseInt((document.getElementById('weight')/* @type {HTMLDivElement} */).innerHTML);
 }
+
 /**
  * @param {(string | string[])} emailAddr - An email address of array of email addresses
  */
 function sendUpdates(emailAddr) {
-    function sendEmail(addr) {
-        // Default to standard delivery if empty
-        // tslint:disable-next-line
-        console.log("Shipping to " + addr + " via " + (deliveryMethod() || 'standard') + " delivery");
-        if (shipWeight() > 100) {
-            // tslint:disable-next-line
-            console.log('WARNING: Oversize package');
-        }
+  function sendEmail(addr) {
+    // Default to standard delivery if empty
+    console.log(`Shipping to ${addr} via ${deliveryMethod() | "standard"} delivery`);
+
+    if (shipWeight() > 100) {
+      console.log("WARNING: Oversize package");
     }
-    // If it's an array, loop over it
-    if (Array.isArray(emailAddr)) {
-        emailAddr.forEach(function (val) {
-            sendEmail(val.trim());
-        });
-    }
-    else {
-        sendEmail(emailAddr.trim());
-    }
+  }
+  // If it's an array, loop over it
+  if (emailAddr.length) {
+    emailAddr.forEach((idx, val) => {
+      sendEmail(val.trim());
+    });
+  } else {
+    sendEmail(emailAddr.trim());
+  }
 }
